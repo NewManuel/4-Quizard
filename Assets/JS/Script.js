6f49c68c48537a0d90f43c51d8f89f5fdf3cda10
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-02-19 14:15:22 -0600

    update

---
+++ Assets/JS/Script.js
@@ -0,0 +1,42 @@
+var questionsList = [
+    {
+        question:'What city was struck by the 9-11 terriost attacks?',
+        answer:{
+            a:'Paris',
+            b:'New York',
+            c:'Bejeing',
+            d:'Durban'
+        },
+        correctAnswer: 'b'
+},
+    {
+        question:'What was the tallest recorded building structure that was later abandoned and unfinshed in the bible?',
+        answer:{
+            a:'Rome Collesium',
+            b:'Burg Khalifa',
+            c:'Empire State Building',
+            d:'The Tower of Babel'
+        },
+        correctAnswer: 'd'
+},
+    {
+        question:'What three things motivate the land migratition of people?',
+        answer:{
+            a:'money, power, and respect',
+            b:'democracy, capitalism, and dollars',
+            c:'economics, resources, and better living',
+            d:'war, famine, and religious freedom'
+        },
+        correctAnswer: 'c'
+},
+    {
+        question:'Is there a difference between a sun and a star, and why?',
+        answer:{
+            a:'Yes, a star is just a bright light in the sky',
+            b:'No, both are essential the same thing',
+            c:"Maybe, depending on the type of star it could be considerd a sun especially if it has planets in it's orbit",
+            d:'Neither, both have nothing to do with eac other'
+        },
+        correctAnswer: 'b'
+}
+];
