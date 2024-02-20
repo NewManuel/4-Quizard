55c246d19e005ada6534b37c025c170cb6e52db2
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-02-20 02:50:11 -0600

    2:50pm-2-20-2024

--- Assets/JS/Script.js
+++ Assets/JS/Script.js
@@ -1,45 +1,45 @@
-var questionsList = [
-    {
-        question:'What city was struck by the 9-11 terriost attacks?',
-        answer:{
-            a:'Paris',
-            b:'New York',
-            c:'Bejeing',
-            d:'Durban'
-        },
-        correctAnswer: 'b'
-},
-    {
-        question:'What was the tallest recorded building structure that was later abandoned and unfinshed in the bible?',
-        answer:{
-            a:'Rome Collesium',
-            b:'Burg Khalifa',
-            c:'Empire State Building',
-            d:'The Tower of Babel'
-        },
-        correctAnswer: 'd'
-},
-    {
-        question:'What three things motivate the land migratition of people?',
-        answer:{
-            a:'money, power, and respect',
-            b:'democracy, capitalism, and dollars',
-            c:'economics, resources, and better living',
-            d:'war, famine, and religious freedom'
-        },
-        correctAnswer: 'c'
-},
-    {
-        question:'Is there a difference between a sun and a star, and why?',
-        answer:{
-            a:'Yes, a star is just a bright light in the sky',
-            b:'No, both are essential the same thing',
-            c:"Maybe, depending on the type of star it could be considerd a sun especially if it has planets in it's orbit",
-            d:'Neither, both have nothing to do with eac other'
-        },
-        correctAnswer: 'b'
-}
-];
+// var questionsList = [
+//     {
+//         question:'What city was struck by the 9-11 terriost attacks?',
+//         answer:{
+//             a:'Paris',
+//             b:'New York',
+//             c:'Bejeing',
+//             d:'Durban'
+//         },
+//         correctAnswer: 'b'
+// },
+//     {
+//         question:'What was the tallest recorded building structure that was later abandoned and unfinshed in the bible?',
+//         answer:{
+//             a:'Rome Collesium',
+//             b:'Burg Khalifa',
+//             c:'Empire State Building',
+//             d:'The Tower of Babel'
+//         },
+//         correctAnswer: 'd'
+// },
+//     {
+//         question:'What three things motivate the land migratition of people?',
+//         answer:{
+//             a:'money, power, and respect',
+//             b:'democracy, capitalism, and dollars',
+//             c:'economics, resources, and better living',
+//             d:'war, famine, and religious freedom'
+//         },
+//         correctAnswer: 'c'
+// },
+//     {
+//         question:'Is there a difference between a sun and a star, and why?',
+//         answer:{
+//             a:'Yes, a star is just a bright light in the sky',
+//             b:'No, both are essential the same thing',
+//             c:"Maybe, depending on the type of star it could be considerd a sun especially if it has planets in it's orbit",
+//             d:'Neither, both have nothing to do with eac other'
+//         },
+//         correctAnswer: 'b'
+// }
+// ];
 
 // This variable is for the nav in the header that says "View Highscore" 
 // var viewHighscore = document.getElementById('score');
@@ -67,8 +67,7 @@ function setTime() {
         if (secondsLeft === 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
-            // Calls function to create and append image
-            sendMessage();
+            // Calls function to create and append image;
         }
 
     }, 1000);
